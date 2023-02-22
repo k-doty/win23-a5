a960b0edd531ff17de40319b87d4462fc7b69624
k-doty <kdoty93@stanford.edu>
2023-02-22 14:36:36 -0800

    updated Makefile and tests

--- calculator/tests/test.sh
+++ calculator/tests/test.sh
@@ -3,25 +3,25 @@
 # Absoulte path to the program
 CALCULATOR=$1
 
-# Test 1: ensure that addition with larger numbers works
+# Test 01: ensure that addition with larger numbers works
 if ! $CALCULATOR 1245 + 4509; then
   echo 'ERROR! A valid run of the calculator (1245 + 4509) failed!'
   exit 1 
 fi
 
-# Test 2: ensure that subtraction resulting in negative value works
+# Test 02: ensure that subtraction resulting in negative value works
 if ! $CALCULATOR 3 - 5; then
   echo 'ERROR! A valid run of the calculator (3 - 5) failed!'
   exit 1 
 fi
 
-# Test 3: ensure that multiplication with negative values works
+# Test 03: ensure that multiplication with negative values works
 if ! $CALCULATOR -3 * -16; then
   echo 'ERROR! A valid run of the calculator (-3 * 16) failed!'
   exit 1 
 fi
 
-# Test 4: ensure that division with larger numbers works 
+# Test 04: ensure that division with larger numbers works 
 if ! $CALCULATOR 1344 / 12; then
   echo 'ERROR! A valid run of the calculator (1344 / 12) failed!'
   exit 1 
